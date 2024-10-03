056c12b6fdb8e272569f4090afc356402614b250
Nikola Vukosavljevic <nvuk0001@student.monash.edu>
2024-10-03 21:05:55 +1000

    tried to fix site, still no luck

--- top3players.js
+++ top3players.js
@@ -1,7 +1,7 @@
 var vg_1 = "homework10.vg.json";
 
   
-  vegaEmbed("#top3", vg_1).then(function(result) {
+  vegaEmbed("#top", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
   
