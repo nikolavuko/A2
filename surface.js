0812c903379fa0694b161ef53dd51b3af06b229c
Nikola Vukosavljevic <nvuk0001@student.monash.edu>
2024-10-08 22:56:43 +1100

    Added surface pie  chart

---
+++ surface.js
@@ -0,0 +1,8 @@
+var vg_3 = "surface.vg.json";
+
+  
+  vegaEmbed("#surface", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+ 
+  
