0bd6e94e0310d7198aeacc25308c1d40b0455641
nikolavuko <111344319+nikolavuko@users.noreply.github.com>
2024-10-09 19:38:14 +1100

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var vg_3 = "test.vg.json";
-
-  
-  vegaEmbed("#test", vg_3).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
-  
