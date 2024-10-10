ec37e280037a1dff71df5f033efff017ff900929
Nikola Vukosavljevic <nvuk0001@student.monash.edu>
2024-10-10 20:24:26 +1100

    top 10 players added

---
+++ top10.js
@@ -0,0 +1,8 @@
+var vg_5 = "top10.vg.json";
+
+  
+  vegaEmbed("#ten", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+ 
+  
