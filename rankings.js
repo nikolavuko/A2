90d3213a56e05ace34b0dc77e6388ab03f52339d
Nikola Vukosavljevic <nvuk0001@student.monash.edu>
2024-10-10 13:58:26 +1100

    adding line chart

---
+++ rankings.js
@@ -0,0 +1,7 @@
+var vg_4 = "rankings.vg.json";
+
+  
+  vegaEmbed("#rank", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+  
