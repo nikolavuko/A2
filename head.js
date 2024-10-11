f2d4dfa0ae2ac5d01155710d77b98eca8ea8d2dc
Nikola Vukosavljevic <nvuk0001@student.monash.edu>
2024-10-11 21:47:26 +1100

    created lollipop instead of bar chart

---
+++ head.js
@@ -0,0 +1,8 @@
+var vg_6 = "head.vg.json";
+
+  
+  vegaEmbed("#head", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+ 
+  
